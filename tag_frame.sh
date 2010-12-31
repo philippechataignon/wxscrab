0922b4a76f7f5cb8cf09df46566dcedd2ac63162
philippe@micro24 <philippe@micro24>
2010-12-31 19:28:15 +0100

    Complete tag_frame.sh de la syntaxe du hook

--- tag_frame.sh
+++ tag_frame.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
+#
+# Ajouter la section suivante dans .hg/hgrc
+#
+#[hooks]
+#commit = ./tag_frame.sh
 hg tip --template="{rev}\t{node|short}\t{date|isodate}\t{author}\n" > client/tag.file
