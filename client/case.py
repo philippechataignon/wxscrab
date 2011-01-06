92da9ff13f21a5486fc9dd6c430e16ea64b6699d
philippe@main <philippe@main>
2011-01-06 11:10:48 +0100

    Conversion complète sans bitmap

--- client/case.py
+++ client/case.py
@@ -11,8 +11,12 @@ import jeton
 import grille
 
 class case(wx.Window) :
-    colors=["#ccc2b8", "#95a5dd", "#6673a1", "#dd9595", "#a16767"]
-    # "#ebe5d4"
+    col_grille=["#ccc2b8", "#95a5dd", "#6673a1", "#dd9595", "#a16767"]
+    col_tirage = "#e9e4e0"
+    col_neutre = "#eeeeee"
+    col_temp = "#ebe5d4"
+    col_pose = "#f6e594"
+    col_tour_jeton = "#b2a56b"
     def __init__(self, parent) :
         self.app = parent.app
         self.settings = self.app.settings
@@ -21,9 +25,9 @@ class case(wx.Window) :
         self.jeton = None
         self.tirage = False
         self.SetDropTarget(dnd.casedroptarget(self))
-        # self.Bind(wx.EVT_SIZE, self.OnSize)
         self.Bind(wx.EVT_PAINT, self.OnPaint)
         self.Bind(wx.EVT_KEY_DOWN, self.OnKey)
+        self.fleche = None
 
     def OnKey(self, e) :
         self.app.frame.grille.OnKey(e)
@@ -35,48 +39,52 @@ class case(wx.Window) :
         self.redraw()
 
     def redraw(self) :
-        bmp = False
-        if not self.tirage :
-            if self.fleche == coord.HOR :
-                self.buffer=self.app.settings.get_img('fl_r')
-                bmp = True
-            elif self.fleche == coord.VER :
-                self.buffer=self.app.settings.get_img('fl_b')
-                bmp = True
-
-        if bmp :
-            wx.BufferedPaintDC(self, self.buffer)
+        dc = wx.PaintDC(self)
+        if self.tirage :
+            dc.SetBrush(wx.Brush(self.col_tirage))
         else :
+            dc.SetBrush(wx.Brush(self.col_grille[self.mult]))
+        # trace le carré de fond
+        dc.SetPen(wx.Pen(self.col_neutre, 1, wx.SOLID))
+        dc.DrawRectangle(0, 0, self.size, self.size) 
+        # puis le jeton éventuel
+        if self.jeton is not None :
             dc = wx.PaintDC(self)
-            if self.tirage :
-                dc.SetBrush(wx.Brush("#e9e4e0"))
-            else :
-                dc.SetBrush(wx.Brush(self.colors[self.mult]))
-            dc.SetPen(wx.Pen("#eeeeee", 1, wx.SOLID))
-            dc.DrawRectangle(0, 0, self.size, self.size) 
-            if self.jeton is not None :
-                dc = wx.PaintDC(self)
-                dc.SetPen(wx.Pen("#b2a56b", 1, wx.SOLID))
-                if self.jeton.status in (jeton.TEMP, jeton.PREPOSE) :
-                    dc.SetBrush(wx.Brush("#ebe5d4"))
-                    if self.jeton.is_joker() :
-                        dc.SetTextForeground(self.settings.get("fontcol_tempjoker"))
-                    else :
-                        dc.SetTextForeground(self.settings.get("fontcol_tempnorm"))
+            dc.SetPen(wx.Pen(self.col_tour_jeton, 1, wx.SOLID))
+            if self.jeton.status in (jeton.TEMP, jeton.PREPOSE) :
+                dc.SetBrush(wx.Brush(self.col_temp))
+                if self.jeton.is_joker() :
+                    dc.SetTextForeground(self.settings.get("fontcol_tempjoker"))
                 else :
-                    dc.SetBrush(wx.Brush("#f6e594"))
-                    if self.jeton.is_joker() :
-                        dc.SetTextForeground(self.settings.get("fontcol_fixejoker"))
-                    else :
-                        dc.SetTextForeground(self.settings.get("fontcol_fixenorm"))
-                dc.DrawRoundedRectangle(1, 1, self.size-2, self.size-2, 4)
-                dc.SetFont(self.settings.get_font())
-                l,h = dc.GetTextExtent(self.jeton.lettre.upper())
-                dc.DrawText(self.jeton.lettre.upper(),(self.size-l)/2,(self.size-h)/2)
-                dc.SetFont(self.settings.get_pointfont())
-                dc.SetTextForeground(self.settings.get("fontcol_points"))
-                l,h = dc.GetTextExtent(self.jeton.point)
-                dc.DrawText(self.jeton.point, self.size-2-l, self.size-2-h)
+                    dc.SetTextForeground(self.settings.get("fontcol_tempnorm"))
+            else :
+                dc.SetBrush(wx.Brush(self.col_pose))
+                if self.jeton.is_joker() :
+                    dc.SetTextForeground(self.settings.get("fontcol_fixejoker"))
+                else :
+                    dc.SetTextForeground(self.settings.get("fontcol_fixenorm"))
+            dc.DrawRoundedRectangle(1, 1, self.size-2, self.size-2, 5)
+            dc.SetFont(self.settings.get_fontnorm())
+            l,h = dc.GetTextExtent(self.jeton.lettre.upper())
+            dc.DrawText(self.jeton.lettre.upper(),(self.size-l)/2,(self.size-h)/2)
+            dc.SetFont(self.settings.get_fontpoint())
+            dc.SetTextForeground(self.settings.get("fontcol_points"))
+            l,h = dc.GetTextExtent(self.jeton.point)
+            dc.DrawText(self.jeton.point, self.size-2-l, self.size-2-h)
+        # ou la flèche
+        elif self.fleche in (coord.HOR, coord.VER) :
+            dc.SetBrush(wx.Brush(self.col_neutre))
+            dc.SetPen(wx.Pen("black", 1, wx.SOLID))
+            dc.DrawCircle(self.size/2, self.size/2, self.size/2-2)
+            dc.SetBrush(wx.Brush("blue"))
+            dc.SetPen(wx.Pen("black", 1, wx.SOLID))
+            pts = [(2.0,6.5), (5,6.5), (5,8), (8.5,5), (5,2), (5,3.5), (2.0,3.5)]
+            if self.fleche == coord.HOR :
+                pts_reel = [(x*self.size/10, y*self.size/10) for (x,y) in pts]
+            elif self.fleche == coord.VER :
+                pts_reel = [(y*self.size/10, x*self.size/10) for (x,y) in pts]
+            dc.DrawPolygon(pts_reel) 
+
 
     def is_vide(self) :
         return self.jeton is None
--- client/settings.py
+++ client/settings.py
@@ -28,17 +28,8 @@ class settings :
         dic_perso = yaml.load(buff)
         for key, item in dic_perso.iteritems() :
             self.dic[key] = item
-        self.img = {}
-        size = self.get('size_jeton')
-        dir = self.get('files_rep_images')
-        for key, nom in self.dic['images'].items() :
-            img = wx.Image(os.path.join(dir, nom) ,wx.BITMAP_TYPE_PNG).Scale(size,size)
-            if key == 'fl_r' :
-                self.img[key] = wx.BitmapFromImage(img.Rotate90(False))
-            else :
-                self.img[key] = wx.BitmapFromImage(img)
-        self.font_norm = wx.Font(self.get('size_font_jeton'),wx.FONTFAMILY_DEFAULT,wx.FONTSTYLE_NORMAL,wx.FONTWEIGHT_BOLD,False,"")
-        self.font_point = wx.Font(self.get('size_font_point'),wx.FONTFAMILY_DEFAULT,wx.FONTSTYLE_NORMAL,wx.FONTWEIGHT_BOLD,False,"")
+        self.font_norm = wx.Font(self.get('size_font_jeton'), wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD)
+        self.font_point = wx.Font(self.get('size_font_point'), wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD)
 
     def write(self) :
         with open(self.file,"w") as f :
@@ -58,16 +49,8 @@ class settings :
             self.dic[key].remove(val)
         self.dic[key].insert(0,val)
  
-    def get_img(self, nom) :
-        return self.img[nom]
-
-    def get_img_copy(self, nom) :
-        bmp_org =  self.img[nom]
-        bmp =  bmp_org.GetSubBitmap(wx.Rect(0, 0, bmp_org.GetWidth(), bmp_org.GetHeight()))
-        return bmp
-
-    def get_font(self) :
+    def get_fontnorm(self) :
         return self.font_norm
 
-    def get_pointfont(self) :
+    def get_fontpoint(self) :
         return self.font_point
