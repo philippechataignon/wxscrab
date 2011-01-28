9fe8fe6356b772f8c52d5e7b296efc5cb7e8d25a
philippe@micro24 <philippe@micro24>
2011-01-28 12:05:14 +0100

    Harmonise pts jeton server et client

--- server/jeton.py
+++ server/jeton.py
@@ -1,5 +1,10 @@
 #! /usr/bin/env python2
 # -*- coding: utf-8 -*-
+
+pts={'A':1,'B':3,'C':3,'D':2,'E':1,'F':4,'G':2,'H':4,'I':1,
+     'J':8,'K':10,'L':1,'M':2,'N':1,'O':1,'P':3,'Q':8,'R':1, 
+     'S':1,'T':1,'U':1,'V':4,'W':10,'X':10,'Y':10,'Z':10,'?':0}
+
 class jeton :
     """
     Définit la notion de jeton
@@ -9,14 +14,10 @@ class jeton :
 
     Utilisé par : case, grille, tirage
     """
-    p = (1,3,3,2,1,4,2,4,1,8,10,1,2,1,1,3,8,1,1,1,1,4,10,10,10,10)
-    l = tuple("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-    points = dict(zip(l,p)) #{'A': 1, 'B': 3...}
-
     def __init__(self, lettre) :
         self.lettre = lettre
-        if self.lettre in jeton.l : 
-            self.point = jeton.points[self.lettre]
+        if self.lettre in pts :
+            self.point = pts[self.lettre]
         else :
             self.point = 0
 
