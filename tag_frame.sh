5e5def5eb0a0e9736a78b632e2295086dc6f7690
philippe@micro14 <philippe@chataignon.com>
2012-05-23 00:16:40 +0200

    Ajoute script tag_frame

--- tag_frame.sh
+++ tag_frame.sh
@@ -1,7 +1,2 @@
 #!/bin/sh
-#
-# Ajouter la section suivante dans .hg/hgrc
-#
-#[hooks]
-#commit = ./tag_frame.sh
-hg tip --template="{rev}\t{node|short}\t{date|isodate}\t{author}\n" > client/tag.file
+git describe --tags > client/tag.file 
