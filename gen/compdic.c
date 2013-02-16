f7a5ad0d3293467b80fffcf89a7497c624953ff8
Philippe Chataignon <philippe@micro24>
2013-02-16 11:19:26 +0100

    Reformate compdic.c, listdic.c et hashtable.c

--- gen/compdic.c
+++ gen/compdic.c
@@ -29,132 +29,101 @@
 #include "hashtable.h"
 #include "dic_internals.h"
 
-//#define DEBUG_LIST
-//#define DEBUG_OUTPUT
-//#define DEBUG_OUTPUT_L2
-#define CHECK_RECURSION
-
-char*
-load_uncompressed(const char* file_name, unsigned int *dic_size)
+char *load_uncompressed(const char *file_name, unsigned int *dic_size)
 {
-  unsigned r;
-  char *uncompressed;
-  FILE* file_desc;
+	unsigned r;
+	char *uncompressed;
+	FILE *file_desc;
 
-  if ((file_desc = fopen (file_name, "r")) == NULL)
-    return NULL;
+	if ((file_desc = fopen(file_name, "r")) == NULL)
+		return NULL;
 
-  if ((uncompressed = (char*)malloc (sizeof(char)*(*dic_size))) == NULL)
-    return NULL;
+	if ((uncompressed = (char *)malloc(sizeof(char) * (*dic_size))) == NULL)
+		return NULL;
 
-  r = fread (uncompressed, 1, *dic_size, file_desc);
-  if (r < *dic_size)
-    {
-      /* \n is 2 chars under M$ */
-      printf("\n");
-      printf("** The number of bytes read is less than the size of the file **\n");
-      printf("** this may be OK if you run a Microsoft OS but not on Unix   **\n");
-      printf("** please check the results.                                  **\n");
-      printf("\n");
-      *dic_size = r;
-    }
+	r = fread(uncompressed, 1, *dic_size, file_desc);
+	if (r < *dic_size) {
+		/* \n is 2 chars under M$ */
+		printf("\n");
+		printf
+		    ("** The number of bytes read is less than the size of the file **\n");
+		printf
+		    ("** this may be OK if you run a Microsoft OS but not on Unix   **\n");
+		printf
+		    ("** please check the results.                                  **\n");
+		printf("\n");
+		*dic_size = r;
+	}
 
-  fclose(file_desc);
-  return uncompressed;
+	fclose(file_desc);
+	return uncompressed;
 }
 
-
-int
-file_length(const char* file_name)
+int file_length(const char *file_name)
 {
-  struct stat stat_buf;
-  if (stat (file_name, &stat_buf) < 0)
-    return - 1;
-  return (int) stat_buf.st_size;
+	struct stat stat_buf;
+	if (stat(file_name, &stat_buf) < 0)
+		return -1;
+	return (int)stat_buf.st_size;
 }
 
-
-void
-skip_init_header(FILE* outfile, Dict_header *header)
+void skip_init_header(FILE * outfile, Dict_header * header)
 {
-  header->unused_1   = 0;
-  header->unused_2   = 0;
-  header->root       = 0;
-  header->nwords     = 0;
-  header->nodesused  = 1;
-  header->edgesused  = 1;
-  header->nodessaved = 0;
-  header->edgessaved = 0;
+	header->unused_1 = 0;
+	header->unused_2 = 0;
+	header->root = 0;
+	header->nwords = 0;
+	header->nodesused = 1;
+	header->edgesused = 1;
+	header->nodessaved = 0;
+	header->edgessaved = 0;
 
-  fwrite (header, sizeof(Dict_header), 1, outfile);
+	fwrite(header, sizeof(Dict_header), 1, outfile);
 }
 
-
-void
-fix_header(FILE* outfile, Dict_header* header)
+void fix_header(FILE * outfile, Dict_header * header)
 {
-  strcpy(header->ident,_COMPIL_KEYWORD_);
-  header->root = header->edgesused;
-  rewind (outfile);
-  fwrite (header, sizeof(Dict_header), 1, outfile);
+	strncpy(header->ident, _COMPIL_KEYWORD_, sizeof( _COMPIL_KEYWORD_));
+	header->root = header->edgesused;
+	rewind(outfile);
+	fwrite(header, sizeof(Dict_header), 1, outfile);
 }
 
-
-void
-print_header_info(Dict_header *header)
+void print_header_info(Dict_header * header)
 {
-  printf("============================\n");
-  printf("keyword length %d bytes\n",(int)strlen(_COMPIL_KEYWORD_));
-  printf("keyword size   %ld bytes\n",sizeof(_COMPIL_KEYWORD_));
-  printf("header size    %ld bytes\n",sizeof(Dict_header));
-  printf("\n");
-  printf("%d words\n",header->nwords);
-  printf("\n");
-  printf("root : %7d (edge)\n",header->root);
-  printf("root : %7ld (byte)\n",header->root * sizeof(Dawg_edge));
-  printf("\n");
-  printf("nodes : %d+%d\n",header->nodesused, header->nodessaved);
-  printf("edges : %d+%d\n",header->edgesused, header->edgessaved);
-  printf("============================\n");
+	printf("============================\n");
+	printf("keyword length %d bytes\n", (int)strlen(_COMPIL_KEYWORD_));
+	printf("keyword size   %ld bytes\n", sizeof(_COMPIL_KEYWORD_));
+	printf("header size    %ld bytes\n", sizeof(Dict_header));
+	printf("\n");
+	printf("%d words\n", header->nwords);
+	printf("\n");
+	printf("root : %7d (edge)\n", header->root);
+	printf("root : %7ld (byte)\n", header->root * sizeof(Dawg_edge));
+	printf("\n");
+	printf("nodes : %d+%d\n", header->nodesused, header->nodessaved);
+	printf("edges : %d+%d\n", header->edgesused, header->edgessaved);
+	printf("============================\n");
 }
 
-
-void
-write_node(Dawg_edge *edges, int size, int num, FILE* outfile)
+void write_node(Dawg_edge * edges, int size, int num, FILE * outfile)
 {
-#ifdef DEBUG_OUTPUT
-  int i;
-  printf("writing %d edges\n",num);
-  for(i=0; i<num; i++)
-    {
-#ifdef DEBUG_OUTPUT_L2
-      printf("ptr=%2d t=%d l=%d f=%d chr=%2d (%c)\n",
-	     edges[i].ptr, edges[i].term, edges[i].last,
-	     edges[i].fill, edges[i].chr, edges[i].chr -1 +'a');
-#endif
-      fwrite (edges+i, sizeof(Dawg_edge), 1, outfile);
-    }
-#else
-  fwrite (edges, size, num, outfile);
-#endif
+	fwrite(edges, size, num, outfile);
 }
 
 #define MAX_STRING_LENGTH 200
-
-
-#define MAX_EDGES 2000
-/* ods3: */
+#define MAX_EDGES 3000
 /* ods4: 1746 */
 
 /* global variables */
-FILE*       global_outfile;
+FILE *global_outfile;
 Dict_header global_header;
-Hash_table  global_hashtable;
+Hash_table global_hashtable;
 
-char        global_stringbuf[MAX_STRING_LENGTH]; /* Space for current string */
-char*       global_endstring;                    /* Marks END of current string */
-char*       global_input;
-char*       global_endofinput;
+char global_stringbuf[MAX_STRING_LENGTH];	/* Space for current string */
+char *global_endstring;		/* Marks END of current string */
+char *global_input;
+char *global_endofinput;
 
 /*
  * Makenode takes a prefix (as postion relative to stringbuf) and
@@ -163,174 +132,151 @@ char*       global_endofinput;
  * to stringbuf) indicating how much of prefix is matched in the
  * input.
  */
-#ifdef CHECK_RECURSION
-int current_rec =0;
+int current_rec = 0;
 int max_rec = 0;
-#endif
 
-unsigned int
-makenode(char *prefix)
+unsigned int makenode(char *prefix)
 {
-  int    numedges;
-  Dawg_edge  edges[MAX_EDGES];
-  Dawg_edge *edgeptr = edges;
-  unsigned  *saved_position;
+	int numedges;
+	Dawg_edge edges[MAX_EDGES];
+	Dawg_edge *edgeptr = edges;
+	unsigned *saved_position;
 
-#ifdef CHECK_RECURSION
-  current_rec++;
-  if (current_rec > max_rec)
-    max_rec = current_rec;
-#endif
+	current_rec++;
+	if (current_rec > max_rec)
+		max_rec = current_rec;
 
-  while (prefix == global_endstring)
-    {
-      /* More edges out of node */
-      edgeptr->ptr  = 0;
-      edgeptr->term = 0;
-      edgeptr->last = 0;
-      edgeptr->fill = 0;
-      edgeptr->chr  = 0;
+	while (prefix == global_endstring) {
+		/* More edges out of node */
+		edgeptr->ptr = 0;
+		edgeptr->term = 0;
+		edgeptr->last = 0;
+		edgeptr->fill = 0;
+		edgeptr->chr = 0;
 
-      (*(edgeptr++)).chr = (*global_endstring++ = *global_input++) & CHAR;
-      if (*global_input == '\n')                 /* End of a word */
-        {
-          global_header.nwords++;
-          edgeptr[-1].term = 1;                  /* Mark edge as word */
-          *global_endstring++ = *global_input++; /* Skip \n */
-          if (global_input == global_endofinput) /* At end of input? */
-            break;
+		(*(edgeptr++)).chr = (*global_endstring++ =
+				      *global_input++) & CHAR;
+		if (*global_input == '\n') {	/* End of a word */
+			global_header.nwords++;
+			edgeptr[-1].term = 1;	/* Mark edge as word */
+			*global_endstring++ = *global_input++;	/* Skip \n */
+			if (global_input == global_endofinput)	/* At end of input? */
+				break;
 
-	  global_endstring = global_stringbuf;
-	  while(*global_endstring == *global_input)
-	    {
-	       global_endstring++;
-	       global_input++;
-	    }
-        }
-      /* make dawg pointed to by this edge */
-      edgeptr[-1].ptr = makenode(prefix + 1);
-    }
+			global_endstring = global_stringbuf;
+			while (*global_endstring == *global_input) {
+				global_endstring++;
+				global_input++;
+			}
+		}
+		/* make dawg pointed to by this edge */
+		edgeptr[-1].ptr = makenode(prefix + 1);
+	}
 
-  numedges = edgeptr - edges;
-  if (numedges == 0)
-    {
-#ifdef CHECK_RECURSION
-      current_rec --;
-#endif
-      return 0;             /* Special node zero - no edges */
-    }
+	numedges = edgeptr - edges;
+	if (numedges == 0) {
+		current_rec--;
+		return 0;	/* Special node zero - no edges */
+	}
 
-  edgeptr[-1].last = 1;   /* Mark the last edge */
+	edgeptr[-1].last = 1;	/* Mark the last edge */
 
-  saved_position = (unsigned int*) hash_find (global_hashtable,
-					      (void*)edges,
-					      numedges*sizeof(Dawg_edge));
-  if (saved_position)
-    {
-      global_header.edgessaved += numedges;
-      global_header.nodessaved++;
+	saved_position = (unsigned int *)hash_find(global_hashtable,
+						   (void *)edges,
+						   numedges *
+						   sizeof(Dawg_edge));
+	if (saved_position) {
+		global_header.edgessaved += numedges;
+		global_header.nodessaved++;
 
-#ifdef CHECK_RECURSION
-      current_rec --;
-#endif
-      return *saved_position;
-    }
-  else
-    {
-      unsigned int node_pos;
+		current_rec--;
+		return *saved_position;
+	} else {
+		unsigned int node_pos;
 
-      node_pos = global_header.edgesused;
-      hash_add(global_hashtable,
-	       (void*)edges,numedges*sizeof(Dawg_edge),
-	       (void*)(&global_header.edgesused),sizeof(global_header.edgesused));
-      global_header.edgesused += numedges;
-      global_header.nodesused++;
-      write_node (edges, sizeof(Dawg_edge), numedges, global_outfile);
+		node_pos = global_header.edgesused;
+		hash_add(global_hashtable,
+			 (void *)edges, numedges * sizeof(Dawg_edge),
+			 (void *)(&global_header.edgesused),
+			 sizeof(global_header.edgesused));
+		global_header.edgesused += numedges;
+		global_header.nodesused++;
+		write_node(edges, sizeof(Dawg_edge), numedges, global_outfile);
 
-#ifdef CHECK_RECURSION
-      current_rec --;
-#endif
-      return node_pos;
-    }
+		current_rec--;
+		return node_pos;
+	}
 }
 
-
-
-
-int
-main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-  int dicsize;
-  unsigned int udicsize;
-  char *uncompressed;
-  Dawg_edge rootnode = {0,0,0,0,0};
-  Dawg_edge specialnode = {0,0,0,0,0};
+	int dicsize;
+	unsigned int udicsize;
+	char *uncompressed;
+	Dawg_edge rootnode = { 0, 0, 0, 0, 0 };
+	Dawg_edge specialnode = { 0, 0, 0, 0, 0 };
 
-  char* outfilename;
-  char outfilenamedefault[] = "dic.dawg";
-  clock_t starttime, endtime;
+	char *outfilename;
+	char outfilenamedefault[] = "dic.dawg";
+	clock_t starttime, endtime;
 
-  if (argc < 2)
-    {
-      fprintf(stderr,"usage: %s uncompressed_dic [compressed_dic]\n",argv[0]);
-      exit(1);
-    }
+	if (argc < 2) {
+		fprintf(stderr, "usage: %s uncompressed_dic [compressed_dic]\n",
+			argv[0]);
+		exit(1);
+	}
 
-  dicsize = file_length (argv[1]);
-  if (dicsize < 0)
-    {
-      fprintf(stderr,"Cannot stat uncompressed dictionary %s\n",argv[1]);
-      exit(1);
-    }
+	dicsize = file_length(argv[1]);
+	if (dicsize < 0) {
+		fprintf(stderr, "Cannot stat uncompressed dictionary %s\n",
+			argv[1]);
+		exit(1);
+	}
 
-  udicsize = (unsigned int)dicsize;
+	udicsize = (unsigned int)dicsize;
 
-  outfilename = (argc == 3) ? argv[2] : outfilenamedefault;
+	outfilename = (argc == 3) ? argv[2] : outfilenamedefault;
 
-  if ((global_outfile = fopen (outfilename,"wb")) == NULL)
-    {
-      fprintf(stderr,"Cannot open output file %s\n",outfilename);
-      exit(1);
-    }
+	if ((global_outfile = fopen(outfilename, "wb")) == NULL) {
+		fprintf(stderr, "Cannot open output file %s\n", outfilename);
+		exit(1);
+	}
 
-  if ((uncompressed = load_uncompressed (argv[1], &udicsize)) == NULL)
-    {
-      fprintf(stderr,"Cannot load uncompressed dictionary into memory\n");
-      exit(1);
-    }
+	if ((uncompressed = load_uncompressed(argv[1], &udicsize)) == NULL) {
+		fprintf(stderr,
+			"Cannot load uncompressed dictionary into memory\n");
+		exit(1);
+	}
 
-  global_input = uncompressed;
-  global_endofinput = global_input + dicsize;
+	global_input = uncompressed;
+	global_endofinput = global_input + dicsize;
 
 #define SCALE 0.6
-  global_hashtable = hash_init(dicsize * SCALE);
+	global_hashtable = hash_init(dicsize * SCALE);
 #undef SCALE
 
-  skip_init_header(global_outfile,&global_header);
+	skip_init_header(global_outfile, &global_header);
 
-  specialnode.last = 1;
-  write_node(&specialnode,sizeof(specialnode),1,global_outfile);
-  /*
-   * Call makenode with null (relative to stringbuf) prefix;
-   * Initialize string to null; Put index of start node on output
-   */
-  starttime=clock();
-  rootnode.ptr = makenode(global_endstring = global_stringbuf);
-  endtime=clock();
-  write_node(&rootnode,sizeof(rootnode),1,global_outfile);
+	specialnode.last = 1;
+	write_node(&specialnode, sizeof(specialnode), 1, global_outfile);
+	/*
+	 * Call makenode with null (relative to stringbuf) prefix;
+	 * Initialize string to null; Put index of start node on output
+	 */
+	starttime = clock();
+	rootnode.ptr = makenode(global_endstring = global_stringbuf);
+	endtime = clock();
+	write_node(&rootnode, sizeof(rootnode), 1, global_outfile);
 
-  fix_header(global_outfile,&global_header);
+	fix_header(global_outfile, &global_header);
 
-  print_header_info(&global_header);
-  hash_destroy(global_hashtable);
-  free(uncompressed);
-  fclose(global_outfile);
+	print_header_info(&global_header);
+	hash_destroy(global_hashtable);
+	free(uncompressed);
+	fclose(global_outfile);
 
-  printf(" Elapsed time is                 : %f s\n", 1.0*(endtime-starttime) / CLOCKS_PER_SEC);
-#ifdef CHECK_RECURSION
-  printf(" Maximum recursion level reached : %d\n",max_rec);
-#endif
-  return 0;
+	printf(" Elapsed time is                 : %f s\n",
+	       1.0 * (endtime - starttime) / CLOCKS_PER_SEC);
+	printf(" Maximum recursion level reached : %d\n", max_rec);
+	return 0;
 }
-
-
--- gen/hashtable.c
+++ gen/hashtable.c
@@ -24,134 +24,113 @@
 #include "hashtable.h"
 
 typedef struct _Hash_node {
-  struct _Hash_node *next;
-  void* key;
-  unsigned int keysize;
-  void* value;
-  unsigned int valuesize;
+	struct _Hash_node *next;
+	void *key;
+	unsigned int keysize;
+	void *value;
+	unsigned int valuesize;
 } Hash_node;
 
 struct _Hash_table {
-  unsigned int size;
-  Hash_node** nodes;
+	unsigned int size;
+	Hash_node **nodes;
 };
 
-
-Hash_table
-hash_init(unsigned int size)
+Hash_table hash_init(unsigned int size)
 {
-  Hash_table ht;
+	Hash_table ht;
 
-  ht = (Hash_table) calloc(1,sizeof(struct _Hash_table));
-  ht->size = size;
-  ht->nodes = (Hash_node  **) calloc (size, sizeof (Hash_node*));
-  return ht;
+	ht = (Hash_table) calloc(1, sizeof(struct _Hash_table));
+	ht->size = size;
+	ht->nodes = (Hash_node **) calloc(size, sizeof(Hash_node *));
+	return ht;
 }
 
-void
-hash_rec_free(Hash_node* node)
+void hash_rec_free(Hash_node * node)
 {
-  if (node)
-    {
-      if (node->next)
-	hash_rec_free(node->next);
-      if (node->key)
-	free(node->key);
-      if (node->value)
-	free(node->value);
-      free(node);
-    }
-}
-
-int
-hash_destroy(Hash_table hashtable)
-{
-  unsigned int i;
-  if (hashtable)
-    {
-      for(i=0; i<hashtable->size; i++)
-	if (hashtable->nodes[i])
-	  hash_rec_free(hashtable->nodes[i]);
-      if (hashtable->nodes)
-	free(hashtable->nodes);
-      free(hashtable);
-    }
-  return 0;
-}
-
-
-static unsigned int
-hash_key(Hash_table hashtable, void* ptr, unsigned int size)
-{
-  unsigned int i;
-  unsigned int key = 0;
-
-  if (size % 4 == 0)
-    {
-      unsigned int *v = (unsigned int*)ptr;
-      for (i = 0; i < (size / 4); i++)
-	key ^= (key << 3) ^ (key >> 1) ^ v[i];
-    }
-  else
-    {
-      unsigned char *v = (unsigned char*)ptr;
-      for (i = 0; i < size; i++)
-	key ^= (key << 3) ^ (key >> 1) ^ v[i];
-    }
-  key %= hashtable->size;
-  return key;
-}
-
-
-void*
-hash_find(Hash_table hashtable, void* key, unsigned int keysize)
-{
-  Hash_node *entry;
-  unsigned int h_key;
-
-  h_key = hash_key(hashtable,key,keysize);
-  for (entry = hashtable->nodes[h_key]; entry; entry = entry -> next)
-    {
-      if ((entry -> keysize == keysize) &&
-	  (memcmp(entry->key,key,keysize) == 0))
-	{
-	  return entry->value;
+	if (node) {
+		if (node->next)
+			hash_rec_free(node->next);
+		if (node->key)
+			free(node->key);
+		if (node->value)
+			free(node->value);
+		free(node);
 	}
-    }
-  return NULL;
 }
 
-
-static Hash_node*
-new_entry(void* key, unsigned int keysize, void* value, unsigned int
-	  valuesize)
+int hash_destroy(Hash_table hashtable)
 {
-  Hash_node *n;
-  n = (Hash_node*)calloc(1,sizeof(Hash_node));
-  n->key = (void*)malloc(keysize);
-  n->value = (void*)malloc(valuesize);
-  n->keysize = keysize;
-  n->valuesize = valuesize;
-  memcpy(n->key,key,keysize);
-  memcpy(n->value,value,valuesize);
-  return n;
+	unsigned int i;
+	if (hashtable) {
+		for (i = 0; i < hashtable->size; i++)
+			if (hashtable->nodes[i])
+				hash_rec_free(hashtable->nodes[i]);
+		if (hashtable->nodes)
+			free(hashtable->nodes);
+		free(hashtable);
+	}
+	return 0;
 }
 
+static unsigned int hash_key(Hash_table hashtable, void *ptr, unsigned int size)
+{
+	unsigned int i;
+	unsigned int key = 0;
+
+	if (size % 4 == 0) {
+		unsigned int *v = (unsigned int *)ptr;
+		for (i = 0; i < (size / 4); i++)
+			key ^= (key << 3) ^ (key >> 1) ^ v[i];
+	} else {
+		unsigned char *v = (unsigned char *)ptr;
+		for (i = 0; i < size; i++)
+			key ^= (key << 3) ^ (key >> 1) ^ v[i];
+	}
+	key %= hashtable->size;
+	return key;
+}
+
+void *hash_find(Hash_table hashtable, void *key, unsigned int keysize)
+{
+	Hash_node *entry;
+	unsigned int h_key;
+
+	h_key = hash_key(hashtable, key, keysize);
+	for (entry = hashtable->nodes[h_key]; entry; entry = entry->next) {
+		if ((entry->keysize == keysize) &&
+		    (memcmp(entry->key, key, keysize) == 0)) {
+			return entry->value;
+		}
+	}
+	return NULL;
+}
+
+static Hash_node *new_entry(void *key, unsigned int keysize, void *value,
+			    unsigned int valuesize)
+{
+	Hash_node *n;
+	n = (Hash_node *) calloc(1, sizeof(Hash_node));
+	n->key = (void *)malloc(keysize);
+	n->value = (void *)malloc(valuesize);
+	n->keysize = keysize;
+	n->valuesize = valuesize;
+	memcpy(n->key, key, keysize);
+	memcpy(n->value, value, valuesize);
+	return n;
+}
 
 int
 hash_add(Hash_table hashtable,
-	 void* key, unsigned int keysize, 
-	 void* value, unsigned int valuesize)
+	 void *key, unsigned int keysize, void *value, unsigned int valuesize)
 {
-  Hash_node *entry;
-  unsigned int h_key;
+	Hash_node *entry;
+	unsigned int h_key;
 
-  h_key = hash_key(hashtable,key,keysize);
-  entry = new_entry(key,keysize,value,valuesize);
-  entry->next = hashtable->nodes[h_key];
-  hashtable->nodes[h_key] = entry;
+	h_key = hash_key(hashtable, key, keysize);
+	entry = new_entry(key, keysize, value, valuesize);
+	entry->next = hashtable->nodes[h_key];
+	hashtable->nodes[h_key] = entry;
 
-  return 0;
+	return 0;
 }
-
-
