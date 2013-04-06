f14c8d441c550052ead2b26673e0b63c7f719544
Username <user@example.com>
2013-04-06 15:27:42 +0200

    Adapte script tmux à virtualenv

--- server/tmux_script.sh
+++ server/tmux_script.sh
@@ -1,8 +1,16 @@
-#!/bin/sh
-#
-# cd /home/scrabble/wxscrab2/server
-# su -c "./tmux_script.sh" scrabble
-#
-tmux new-session -s scrab -n normal      -d  './go.py  -p1989 -l'
-sleep 2
-tmux new-window  -a -t normal -n topping -d  './go.py  -p1991 -o -c90 -i10 -l'
+#!/bin/zsh
+export VIRTUAL_ENV="/home/scrabble/ve"
+cd $VIRTUAL_ENV
+tmux new-session -s scrab -n normal -d 
+tmux send-keys 'cd wxscrab/server' 'C-m'
+tmux send-keys '$VIRTUAL_ENV/bin/python go.py -p1989 -l' 'C-m'
+sleep 1
+tmux new-window  -a -t normal -n topping -d 
+tmux select-window -n
+tmux send-keys 'cd wxscrab/server' 'C-m'
+tmux send-keys '$VIRTUAL_ENV/bin/python go.py -p1991 -o -c90 -i10 -l' 'C-m'
+sleep 1
+tmux new-window  -a -t normal -n public -d 
+tmux select-window -n
+tmux send-keys 'cd wxscrab/server' 'C-m'
+tmux send-keys '$VIRTUAL_ENV/bin/python go.py -p12345' 'C-m'
