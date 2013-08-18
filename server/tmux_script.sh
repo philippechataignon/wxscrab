a88f290e28ae7fe5b345e9a4a1857bb4e9762fdd
Philippe Chataignon <philippe@micro29.lasout.local>
2013-08-18 13:29:43 +0200

    Supprime sleep dans tmux suite à utilisation random

--- server/tmux_script.sh
+++ server/tmux_script.sh
@@ -1,15 +1,13 @@
 #!/bin/zsh
-export VIRTUAL_ENV="/home/scrabble/ve"
+export VIRTUAL_ENV="/home/philippe/vescrab"
 cd $VIRTUAL_ENV
 tmux new-session -s scrab -n normal -d 
 tmux send-keys 'cd wxscrab/server' 'C-m'
 tmux send-keys '$VIRTUAL_ENV/bin/python go.py -p1989 -l' 'C-m'
-sleep 1
 tmux new-window  -a -t normal -n topping -d 
 tmux select-window -n
 tmux send-keys 'cd wxscrab/server' 'C-m'
 tmux send-keys '$VIRTUAL_ENV/bin/python go.py -p1991 -o -c90 -i10 -l' 'C-m'
-sleep 1
 tmux new-window  -a -t normal -n public -d 
 tmux select-window -n
 tmux send-keys 'cd wxscrab/server' 'C-m'
