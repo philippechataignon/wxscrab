a069fe5349b2619209c1faa9dbf8690053f7b744
Philippe Chataignon <philippe@ldlc>
2019-07-31 17:11:29 +0200

    Remonte gen d'un répertoire

--- gen/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-docker build -t philippechataignon/wxscrab_gen:build .
-docker create --name wxscrab_gen philippechataignon/wxscrab_gen:build
-docker cp wxscrab_gen:gen/gen_part .
-docker cp wxscrab_gen:gen/dist .
